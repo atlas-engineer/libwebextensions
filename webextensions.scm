485cae0e0e28693256d54d1e674e999b19fd2a8c
aartaka <aartaka@protonmail.com>
2022-09-01 11:33:05 +0400

    Prototype calling Scheme procedures with C pointers.
    It works!

--- webextensions.scm
+++ webextensions.scm
@@ -6,16 +6,32 @@
 ;; Implies that the .so this is called from is linked against WebKit.
 (define lib (load-foreign-library #f #:search-system-paths? #t))
 
-;; (define (g-signal-connect instance signal handler)
-;;   ((foreign-library-function lib "g_signal_connect_data"
-;;                              #:arg-types (list '* '* '* '* '* int)
-;;                              #:return-type '*)
-;;    instance
-;;    (if (pointer? signal)
-;;        signal
-;;        (string->pointer signal))
-;;    handler
-;;    %null-pointer %null-pointer 0))
+(define* (g-signal-connect instance signal handler #:optional (data #f))
+  ((foreign-library-function lib "g_signal_connect_data"
+                             #:arg-types (list '* '* '* '* '* int)
+                             #:return-type '*)
+   instance
+   (if (pointer? signal)
+       signal
+       (string->pointer signal))
+   handler (or data %null-pointer) %null-pointer 0))
+
+(define* (g-print format-string . args)
+  (apply (pointer->procedure void
+                             (foreign-library-pointer lib "g_print")
+                             (cons '*
+                                   (map (lambda (a)
+                                          (cond
+                                           ((or (string? a)
+                                                (pointer? a))
+                                            '*)
+                                           ((number? a)
+                                            int64)))
+                                        args)))
+         (string->pointer format-string)
+         (map (lambda (a) (if (string? a)
+                              (string->pointer a)
+                              a)) args)))
 
 (define jsc-make-context
   (foreign-library-function lib "jsc_context_new" #:return-type '*))
@@ -23,22 +39,22 @@
 (define (jsc-make-null context)
   ((foreign-library-function lib "jsc_value_new_null"
                              #:return-type '*
-                             #:arg-types (list '*))
+                             #:arg-types '(*))
    context))
 
-;; (define (jsc-null? arg)
-;;   (= 1 ((foreign-library-function lib "jsc_value_is_null"
-;;                                   #:return-type unsigned-int
-;;                                   #:arg-types (list '*))
-;;         arg)))
+(define (jsc-null? arg)
+  (= 1 ((foreign-library-function lib "jsc_value_is_null"
+                                  #:return-type unsigned-int
+                                  #:arg-types '(*))
+        arg)))
 
-;; (define (json->jsc json #:optional (context (make-jsc-context)))
-;;   ((foreign-library-function lib "jsc_value_new_from_json"
-;;                              #:return-type '*
-;;                              #:arg-types (list '* '*))
-;;    context (if (pointer? json)
-;;                json
-;;                (string->pointer json))))
+(define* (json->jsc json #:optional (context (jsc-make-context)))
+  ((foreign-library-function lib "jsc_value_new_from_json"
+                             #:return-type '*
+                             #:arg-types '(* *))
+   context (if (pointer? json)
+               json
+               (string->pointer json))))
 
 (define (jsc->json jsc-value)
   (pointer->string
@@ -47,12 +63,27 @@
                               #:arg-types (list '* int))
     jsc-value 0)))
 
-;; (define (jsc->string jsc-value)
-;;   (pointer->string
-;;    ((foreign-library-function lib "jsc_value_to_string"
-;;                               #:return-type '*
-;;                               #:arg-types (list '*)) jsc-value)))
+(define (jsc->string jsc-value)
+  (pointer->string
+   ((foreign-library-function lib "jsc_value_to_string"
+                              #:return-type '*
+                              #:arg-types (list '*)) jsc-value)))
 
-(define (entry-webextensions)
-  (display (jsc->json (jsc-make-null (jsc-make-context))))
-  (display "\n"))
+(define (page-id page)
+  ((foreign-library-function lib "webkit_web_page_get_id"
+                             #:return-type uint64
+                             #:arg-types '(*))
+   page))
+
+(define *page* #f)
+
+(define (entry-webextensions extension-ptr)
+  (g-signal-connect
+   extension-ptr "page-created"
+   (procedure->pointer
+    void
+    (lambda (extension page)
+      (set! *page* page)
+      (g-print "Page %i created!\n" (page-id page)))
+    '(* *)))
+  (display "WebExtensions Library handlers installed.\n"))
