94283987ea25536f819c7eab30414b6a70e48948
aartaka <aartaka@protonmail.com>
2022-09-01 12:15:09 +0400

    webextensions(foreign-fn): Add a helper.

--- webextensions.scm
+++ webextensions.scm
@@ -3,11 +3,18 @@
   #:use-module (system foreign-library)
   #:export (entry-webextensions))
 
-;; Implies that the .so this is called from is linked against WebKit.
-(define lib (load-foreign-library #f #:search-system-paths? #t))
+;; When developing, try:
+;; (define lib (load-foreign-library "/path/to/lib/libwebkit2gtk-4.1.so"))
+(define lib #f)
+
+(define (foreign-fn name args return-type)
+  (foreign-library-function
+   lib name
+   #:return-type return-type
+   #:arg-types args))
 
 (define jsc-make-context
-  (foreign-library-function lib "jsc_context_new" #:return-type '*))
+  (foreign-fn "jsc_context_new" '() '*))
 
 (define (jsc-make-null context)
   ((foreign-library-function lib "jsc_value_new_null"
@@ -16,36 +23,25 @@
    context))
 
 (define (jsc-null? arg)
-  (= 1 ((foreign-library-function lib "jsc_value_is_null"
-                                  #:return-type unsigned-int
-                                  #:arg-types '(*))
-        arg)))
+  (= 1 ((foreign-fn "jsc_value_is_null" '(*) unsigned-int) arg)))
 
 (define* (json->jsc json #:optional (context (jsc-make-context)))
-  ((foreign-library-function lib "jsc_value_new_from_json"
-                             #:return-type '*
-                             #:arg-types '(* *))
+  ((foreign-fn "jsc_value_new_from_json" '(* *) '*)
    context (if (pointer? json)
                json
                (string->pointer json))))
 
 (define (jsc->json jsc-value)
   (pointer->string
-   ((foreign-library-function lib "jsc_value_to_json"
-                              #:return-type '*
-                              #:arg-types (list '* int))
+   ((foreign-fn"jsc_value_to_json" (list '* int) '*)
     jsc-value 0)))
 
 (define (jsc->string jsc-value)
   (pointer->string
-   ((foreign-library-function lib "jsc_value_to_string"
-                              #:return-type '*
-                              #:arg-types (list '*)) jsc-value)))
+   ((foreign-fn "jsc_value_to_string" (list '*) '*) jsc-value)))
 
 (define* (g-signal-connect instance signal handler #:optional (data #f))
-  ((foreign-library-function lib "g_signal_connect_data"
-                             #:arg-types (list '* '* '* '* '* int)
-                             #:return-type '*)
+  ((foreign-fn "g_signal_connect_data" (list '* '* '* '* '* int) '*)
    instance
    (if (pointer? signal)
        signal
@@ -67,13 +63,11 @@
          (string->pointer format-string)
          (map (lambda (a) (if (string? a)
                               (string->pointer a)
-                              a)) args)))
+                              a))
+              args)))
 
 (define (page-id page)
-  ((foreign-library-function lib "webkit_web_page_get_id"
-                             #:return-type uint64
-                             #:arg-types '(*))
-   page))
+  ((foreign-fn "webkit_web_page_get_id" '(*) uint64) page))
 
 (define *page* #f)
 
