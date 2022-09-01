20afe8238d1058b0f2835b27ec3cfb4ffdc363d4
aartaka <aartaka@protonmail.com>
2022-09-01 11:58:01 +0400

    webextensions: Move JSCore bindings earlier.

--- webextensions.scm
+++ webextensions.scm
@@ -6,33 +6,6 @@
 ;; Implies that the .so this is called from is linked against WebKit.
 (define lib (load-foreign-library #f #:search-system-paths? #t))
 
-(define* (g-signal-connect instance signal handler #:optional (data #f))
-  ((foreign-library-function lib "g_signal_connect_data"
-                             #:arg-types (list '* '* '* '* '* int)
-                             #:return-type '*)
-   instance
-   (if (pointer? signal)
-       signal
-       (string->pointer signal))
-   handler (or data %null-pointer) %null-pointer 0))
-
-(define* (g-print format-string . args)
-  (apply (pointer->procedure void
-                             (foreign-library-pointer lib "g_print")
-                             (cons '*
-                                   (map (lambda (a)
-                                          (cond
-                                           ((or (string? a)
-                                                (pointer? a))
-                                            '*)
-                                           ((number? a)
-                                            int64)))
-                                        args)))
-         (string->pointer format-string)
-         (map (lambda (a) (if (string? a)
-                              (string->pointer a)
-                              a)) args)))
-
 (define jsc-make-context
   (foreign-library-function lib "jsc_context_new" #:return-type '*))
 
@@ -68,6 +41,33 @@
    ((foreign-library-function lib "jsc_value_to_string"
                               #:return-type '*
                               #:arg-types (list '*)) jsc-value)))
+
+(define* (g-signal-connect instance signal handler #:optional (data #f))
+  ((foreign-library-function lib "g_signal_connect_data"
+                             #:arg-types (list '* '* '* '* '* int)
+                             #:return-type '*)
+   instance
+   (if (pointer? signal)
+       signal
+       (string->pointer signal))
+   handler (or data %null-pointer) %null-pointer 0))
+
+(define* (g-print format-string . args)
+  (apply (pointer->procedure void
+                             (foreign-library-pointer lib "g_print")
+                             (cons '*
+                                   (map (lambda (a)
+                                          (cond
+                                           ((or (string? a)
+                                                (pointer? a))
+                                            '*)
+                                           ((number? a)
+                                            int64)))
+                                        args)))
+         (string->pointer format-string)
+         (map (lambda (a) (if (string? a)
+                              (string->pointer a)
+                              a)) args)))
 
 (define (page-id page)
   ((foreign-library-function lib "webkit_web_page_get_id"
