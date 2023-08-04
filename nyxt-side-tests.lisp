802501101e811fc2a458c4df0b3d0c9bd18abbeb
Artyom Bologov <aartaka@protonmail.com>
2023-08-04 13:32:30 +0400

    nyxt-side-tests: Add for message testing.

---
+++ nyxt-side-tests.lisp
@@ -0,0 +1,10 @@
+(nyxt:ffi-web-extension-send-message
+ (nyxt:current-buffer)
+ (webkit:webkit-user-message-new "addExtension" (glib:g-variant-new-string "{\"name\":\"test\"}"))
+ nil nil)
+
+(nyxt:ffi-buffer-evaluate-javascript (nyxt:current-buffer) "browser.test.prop" "test")
+
+(nyxt:ffi-buffer-evaluate-javascript (nyxt:current-buffer) "browser.test.prop2" "test")
+
+(nyxt:ffi-buffer-evaluate-javascript (nyxt:current-buffer) "browser.test.method(null).then((v) => browser.test.prop2 = v)" "test")
