089d86fe04a89d2e2fde5da90f7d429f49dc51ac
Artyom Bologov <aartaka@protonmail.com>
2023-09-28 16:16:37 +0400

    webextensions(make-jsc-promise): Safer timeout for the time being.

--- source/webextensions.scm
+++ source/webextensions.scm
@@ -750,7 +750,11 @@ Sends the message with NAME name and ARGS as content."
         var value = check();
         console.log(\"Got \" + value + \" value\");
         if (value === null) {
-            setTimeout(() => rec(success, failure), 10);
+            setTimeout(() => {
+                console.log(\"Timeout fired\");
+                rec(success, failure);
+            },
+                       100);
         } else {
             if (value.hasOwnProperty(\"error\")) {
                 let error = new Error(value.error);
