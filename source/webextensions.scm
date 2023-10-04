b01a8c0a5f1b0e8306bdbe47dac820d21956020f
Artyom Bologov <aartaka@protonmail.com>
2023-10-04 18:45:44 +0400

    webextensions: Add runtime.getBrowserInfo.

--- source/webextensions.scm
+++ source/webextensions.scm
@@ -861,7 +861,8 @@ procedure) return a JSCValue!"
   (list "create" #:method "browser.tabs.create" 2))
 
 (define-api "runtime" "Runtime"
-  (list "getPlatformInfo" #:method "runtime.getPlatformInfo" 1))
+  (list "getPlatformInfo" #:method "runtime.getPlatformInfo" 1)
+  (list "getBrowserInfo" #:method "runtime.getBrowserInfo" 1))
 
 (define (inject-browser context)
   (g-print "Injecting browser into ~s" context)
