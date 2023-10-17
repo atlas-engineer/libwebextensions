686e71ff5650cf1f407c9f6cded87e3611712754
Artyom Bologov <aartaka@protonmail.com>
2023-10-17 17:22:53 +0400

    Revert "webextensions(jsc->number): Use inexact->exact."
    This reverts commit 921a21775fcff63c3f76c5316a1911f3a3c3e8f4.
    
    Should be unnecessary.

--- source/webextensions.scm
+++ source/webextensions.scm
@@ -416,7 +416,8 @@ context it belongs to."
 (define (jsc->number jsc)
   ;; No int32 conversions, because most Guile operations convert
   ;; floats to ints when necessary.
-  (inexact->exact ((foreign-fn "jsc_value_to_double" '(*) double) jsc)))
+  ;; TODO: Maybe call inexact->exact on the result?
+  ((foreign-fn "jsc_value_to_double" '(*) double) jsc))
 
 (define* (make-jsc-boolean value #:optional (context (jsc-context-get/make)))
   ((foreign-fn "jsc_value_new_boolean" (list '* unsigned-int) '*)
